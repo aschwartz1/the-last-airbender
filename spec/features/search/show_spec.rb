95d3e5be08dee3f7a6a6e6d21cf34f0ee69b5340
Alex Schwartz <aschwartz@detaso.com>
2021-04-05 11:48:43 -0500

    Add photos

--- spec/features/search/show_spec.rb
+++ spec/features/search/show_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Search by nation page' do
   describe 'happy path' do
-    xit 'basic page setup' do
-      visit search_path
+    it 'basic page setup' do
+      visit search_path(nation: 'fire_nation')
       expect(current_path).to eq(search_path)
     end
 
@@ -22,6 +22,38 @@ RSpec.describe 'Search by nation page' do
         within('#people-info') do
           expect(page.all('.person').size).to eq(25)
         end
+      end
+
+      it 'shows detailed info for each person' do
+        visit search_path(nation: 'fire_nation')
+
+        persons = page.all(:css, '.person')
+
+        within(persons.first) do
+          expect(page).to have_content('Chan (Fire Nation admiral)')
+          expect(page).to have_content('Ozai')
+          expect(page).to have_content('Earth Kingdom')
+          expect(page).to have_content('Fire Nation Navy')
+        end
+
+        within(persons.last) do
+          expect(page).to have_content('Fire Nation train conductor')
+          expect(page).to have_content('Aang')
+          expect(page).to have_content('Combustion Man')
+          expect(page).to have_content('Fire Nation Fire Nation train')
+        end
+      end
+
+      xit 'shows `none` for allies/enemies if there are none' do
+        # TODO find a person w/ those and make capybara select their .person element?
+      end
+
+      it 'photos for people when it can' do
+        visit search_path(nation: 'fire_nation')
+        photos = page.all(:css, '.person-photo')
+
+        # Not sure how to test for a photo actually existing?
+        expect(photos.size).to eq(24)
       end
     end
   end
